6da8057a2d82ef04ba74f70d47dde059f611d59d
René Schultz Madsen <rm@microting.com>
2017-08-22 15:22:20 +0200

    Adding ther first Travis part.

---
+++ MicrotingService.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace MicrotingService.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            bool value = true;
+
+            Assert.Equal(true, value);
+        }
+    }
+}
